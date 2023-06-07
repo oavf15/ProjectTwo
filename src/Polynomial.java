cde862d9b6900809c18f68542e26ce918ae11c28
Oscar Vasquez <48740780+oavf15@users.noreply.github.com>
2023-06-07 14:28:42 -0400

    Create Term class.

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,2 +1,35 @@
-public class Polynomial {
+import java.util.Iterator;
+
+public class Polynomial implements Comparable<Polynomial>, Iterable<Polynomial.term>{
+    private Node head;
+
+    public static class Term {
+        double coefficient;
+        int exponent;
+
+        public Term(double coefficient, int exponent) {
+            this.coefficient = coefficient;
+            this.exponent = exponent;
+        }
+
+        public String toString() {
+            if (exponent == 0) {
+                return "" + coefficient;
+            } else if (exponent == 1) {
+                return coefficient + "x";
+            } else {
+                return coefficient + "x^" + exponent;
+            }
+        }
+    }
+
+    @Override
+    public int compareTo(Polynomial o) {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Polynomial.term> iterator() {
+        return null;
+    }
 }
