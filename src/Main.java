def7408910ddadae3715982e3b3d0f233acfc47b
Oscar Vasquez <48740780+oavf15@users.noreply.github.com>
2023-06-10 00:13:56 -0400

    Defined comparator lambda.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Comparator;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Comparator<Polynomial> weakOrder = (p1, p2) -> {
+            if (p1.getHighestExponent() < p2.getHighestExponent()) {
+                return -1;
+            } else if (p1.getHighestExponent() > p2.getHighestExponent()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        };
     }
 }
