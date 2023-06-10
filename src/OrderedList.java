f5bad741b4f343db6061d5e0907e81ac5e902630
Oscar Vasquez <48740780+oavf15@users.noreply.github.com>
2023-06-10 00:10:55 -0400

    Update OrderedList.java.

--- src/OrderedList.java
+++ src/OrderedList.java
@@ -1,2 +1,16 @@
+import java.util.Comparator;
+import java.util.List;
+
 public class OrderedList {
+    public static <T extends Comparable<? super T>> boolean checkSorted(List<T> list) {
+        return checkSorted(list, Comparator.naturalOrder());
+    }
+    public static <T> boolean checkSorted(List<T> list, Comparator<? super T> comparator) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
